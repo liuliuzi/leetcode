d33f730814fa8f09809c5c6419a1ee226f61dd2c
yaotliu <yaoting.liu@nokia.com>
2018-08-22 20:21:41 +0800

    241

---
+++ 241.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def diffWaysToCompute(self, input):
+        """
+        :type input: str
+        :rtype: List[int]
+        """
+        def dfs(s, cache) :
+            ops = {'+':lambda x,y:x+y, '-':lambda x,y:x-y, '*':lambda x,y:x*y}
+            if not cache.has_key(s) :
+                ret = []
+                for k, v in enumerate(s) :
+                    if v in '+-*' :
+                        for left in dfs(s[:k], cache) :
+                            for right in dfs(s[k+1:], cache) :
+                                ret.append(ops[v](left,right))
+                if not ret :
+                    ret.append(int(s))
+                cache[s] = ret
+            return cache[s]
+        return dfs(input, {})
+
+
+sol=Solution()
+print sol.diffWaysToCompute("2*3-4*5")
