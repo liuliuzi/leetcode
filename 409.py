daeb59f81354678467ad0f01e5abaa8b666ccd32
yaotliu <yaoting.liu@nokia.com>
2019-04-25 15:24:31 +0800

    409

---
+++ 409.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestPalindrome(self, word):
+        retVal=0
+        charDict={}
+        for node in word:
+            if charDict.has_key(node):
+                charDict[node]=charDict[node]+1
+            else:
+                charDict[node]=1
+        flag=0
+        print charDict
+        for k,v in charDict:
+            if v%2!=0 and flag==0:
+                retVal=retVal+1
+                flag=1
+            retVal=retVal+v/2
+        return retVal
+
+sol=Solution()
+print sol.longestPalindrome("abccccdd")
