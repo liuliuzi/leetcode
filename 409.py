f925a1675500ed3c862165d44ac615b7168af168
yaotliu <yaoting.liu@nokia.com>
2019-04-29 13:30:32 +0800

    409

--- 409.py
+++ 409.py
@@ -9,7 +9,7 @@ class Solution(object):
                 charDict[node]=1
         flag=0
         print charDict
-        for k,v in charDict:
+        for k,v in charDict.iteritems():
             if v%2!=0 and flag==0:
                 retVal=retVal+1
                 flag=1
