8f7465dac5c7b103b77f3f86f353e1cdd61ede9f
Liu <yaoting.liu@nokia-sbell.com>
2019-05-29 15:39:33 +0800

    67

---
+++ 67.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param a, a string
+    # @param b, a string
+    # @return a string
+    def addBinary(self, a, b):
+        result, carry, val = "", 0, 0
+        for i in xrange(max(len(a), len(b))):
+            val = carry
+            if i < len(a):
+                val += int(a[-(i + 1)])
+            if i < len(b):
+                val += int(b[-(i + 1)])
+            carry, val = val / 2, val % 2
+            result += str(val)
+        if carry:
+            result += str(carry)
+        return result[::-1]
