9e5d91cd3d66bf05838dfa7d7bfe05d84849f7b8
Liu <yaoting.liu@nokia-sbell.com>
2019-06-25 09:56:11 +0800

    83

---
+++ 83.py
@@ -0,0 +1,9 @@
+class Solution: 
+    def deleteDuplicates(self, head):
+        cur = head
+        while cur:
+            if cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
