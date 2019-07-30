af7a05bb517b49112e0f83478c1cadc2ae17f188
Liu <yaoting.liu@nokia-sbell.com>
2019-07-30 13:35:24 +0800

    503

---
+++ 503.py
@@ -0,0 +1,20 @@
+'''Example 1:
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number; 
+The second 1's next greater number needs to search circularly, which is also 2.
+'''
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [-1] * len(nums)
+        stack = []
+        for i in range(len(nums)) * 2:
+            while stack and (nums[stack[-1]] < nums[i]):
+                res[stack.pop()] = nums[i]
+            stack.append(i)
+        return res
