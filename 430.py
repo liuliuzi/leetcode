f559fbe9197d13c26325e7ecb4a8d0108323fd52
Liu <yaoting.liu@nokia-sbell.com>
2019-09-11 09:29:19 +0800

    430

---
+++ 430.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def flatten(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head: return None
+        node = head
+        while node:
+            node_next = node.next
+            if node.child:
+                flattened = self.flatten(node.child)
+                node.child = None
+                nextNode = self.appendToList(node, flattened)
+                node = nextNode
+            else:
+                node = node.next
+        return head
+
+    def appendToList(self, node, listToAppendHead):
+        next_node = node.next
+        node.next = listToAppendHead
+        listToAppendHead.prev = node
+        while node.next:
+            node = node.next
+        node.next = next_node
+        if next_node:
+            next_node.prev = node
+        return next_node
