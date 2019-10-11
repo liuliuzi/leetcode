07e5952a04d41ebef09193153b2e0dd6f67568c0
Liu <yaoting.liu@nokia-sbell.com>
2019-10-11 09:59:06 +0800

    531

---
+++ 531.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findLonelyPixel(self, picture):
+        """
+        :type picture: List[List[str]]
+        :rtype: int
+        """
+        rows, cols = [0] * len(picture),  [0] * len(picture[0])
+        for i in range(len(picture)):
+            for j in range(len(picture[0])):
+                if picture[i][j]=='B':
+                    rows[i]+=1
+                    rows[j]+=1
+        result = 0
+        for i in range(len(picture)):
+            if rows[i] == 1:
+                for j in range(len(picture[0])):
+                    result += picture[i][j] == 'B' and cols[j] == 1
+        return result
