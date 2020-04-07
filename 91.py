f4b7726f361282d63ab751af9807c61a0e4d421c
Liu <yaoting.liu@nokia-sbell.com>
2020-04-07 14:48:15 +0800

    91

---
+++ 91.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numDecodings(self, s):
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        for i in range(1, len(dp)):
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+            if i != 1 and '09' < s[i-2:i] < '27':
+                dp[i] += dp[i-2]
+        return dp[-1]
